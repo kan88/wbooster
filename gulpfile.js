5f2142948d9c055783ecec3e019841974b10c369
Евгений <msk9199625687@yandex.ru>
2022-06-21 17:48:56 +0300

    test

--- gulpfile.js
+++ gulpfile.js
@@ -97,7 +97,8 @@ const copy = (done) => {
  gulp.src([
   'source/fonts/*.{woff2,woff}',
   'source/**/*.ico',
-  'source/**/*.manifest'
+  'source/**/*.manifest',
+  'source/*.php'
   ], {
   base: 'source'
   })
